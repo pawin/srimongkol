6e1192cd30ffd5c9d4fff435437c901c32df13f5
Pawin Thepbanchaporn <w.i.n@me.com>
2019-07-20 13:17:55 +0700

    Initial Commit

---
+++ srimongkol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  srimongkol
+//
+//  Created by win on 20/7/19.
+//  Copyright © 2019 Srimongkol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
